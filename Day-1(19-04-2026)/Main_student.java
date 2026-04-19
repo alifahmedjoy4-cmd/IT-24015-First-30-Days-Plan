20463ee7ad6d790aaa86bd03951ff7286123ba20
Alif Ahmed Joy <alifahmedjoy4@gmail.com>
2026-04-20 01:53:44 +0600

    Create Main_student.java

---
+++ Day-1(19-04-2026)/Main_student.java
@@ -0,0 +1,24 @@
+public class Main_student {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.name = "Mohitul Islam";
+        s1.Roll = 65;
+        s1.district = "Tangail";
+        s1.Number = "01611211238";
+        s1.Show();
+
+        Student s2 = new Student();
+        s2.name = "Alif";
+        s2.Roll = 66;
+        s2.district = "Cumilla";
+        s2.Number = "01629277953";
+        s2.Show();
+
+        Student s3 = new Student();
+        s3.name = "Anom";
+        s3.Roll = 67;
+        s3.district = "Chittagong";
+        s3.Number = "01749123456";
+        s3.Show();
+    }
+}
