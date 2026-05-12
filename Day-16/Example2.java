fa236a2c5872d3dfd583d2c2b3282900694944fb
Alif Ahmed Joy <alifahmedjoy4@gmail.com>
2026-05-13 00:49:50 +0600

    Update Example2.java

--- Day-16/Example2.java
+++ Day-16/Example2.java
@@ -1,46 +1,15 @@
+import java.io.FileInputStream;
+import java.io.IOException;
 
-class Base {
-
-    String name;
-
-    // Constructor 1
-    Base() {
-        this("");
-
-        System.out.println("No-argument constructor of base class");
-    }
-
-    // Constructor 2
-    Base(String name) {
-        this.name = name;
-
-        System.out.println("Calling parameterized constructor of base");
-    }
-}
-\
-class Derived extends Base {
-
-    // Constructor 3
-    Derived() {
-
-        System.out.println("No-argument constructor of derived");
-    }
-
-    // Constructor 4
-    Derived(String name) {
-
-        // Calls parameterized constructor of Base class
-        super(name);
-
-        System.out.println("Calling parameterized constructor of derived");
-    }
-}
-
-// Main class
 public class Example2 {
-    public static void main(String[] args) {
-        Derived obj = new Derived("test");
+    public static void main(String[] args) throws IOException {
+        FileInputStream in = new FileInputStream("output.txt");
 
-     
+        int data;
+        while ((data = in.read()) != -1) {
+            System.out.print((char) data);
+        }
+
+        in.close();
     }
 }
