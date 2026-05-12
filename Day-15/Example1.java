a3663328d01f0d28f4d16b3c8edfc75c9fe624b2
Alif Ahmed Joy <alifahmedjoy4@gmail.com>
2026-05-13 00:46:41 +0600

    Update Example1.java

--- Day-15/Example1.java
+++ Day-15/Example1.java
@@ -1,89 +1,19 @@
-/*
-    This program explains two pillars of OOP:
-    1. Encapsulation
-    2. Inheritance
-*/
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
-// Private data + Public methods = Encapsulation
-
-class Person {
-    // private variables 
-    private String name;
-    private int age;
-
-    // constructor
-    public Person(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    // setter 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    // getter 
-    public String getName() {
-        return name;
-    }
-
-    // setter 
-    public void setAge(int age) {
-        if (age > 0) {
-            this.age = age;
-        }
-    }
-
-    // getter 
-    public int getAge() {
-        return age;
-    }
-
-    // display 
-    public void displayPersonInfo() {
-        System.out.println("Name : " + name);
-        System.out.println("Age  : " + age);
-    }
-}
-
-// Student class inherits Person class
-
-class Student extends Person {
-
-    private String department;
-
-    // constructor
-    public Student(String name, int age, String department) {
-        super(name, age); // calling parent constructor
-        this.department = department;
-    }
-
-    // method for displaying student info
-    public void displayStudentInfo() {
-        displayPersonInfo(); // inherited method
-        System.out.println("Department : " + department);
-    }
-}
-
-
-// Main Class
 public class Example1 {
+    public static void main(String[] args) throws IOException {
+        FileOutputStream out = new FileOutputStream("data.bin");
+        out.write(65);
+        out.write(66);
+        out.close();
 
-    public static void main(String[] args) {
-
-        // Creating Student object
-        Student s1 = new Student("Rakib", 21, "ICT");
-
-        // Accessing methods
-        s1.displayStudentInfo();
-
-        System.out.println("\nUsing Getter:");
-        System.out.println("Student Name: " + s1.getName());
-
-        // Updating value using setter
-        s1.setAge(22);
-
-        System.out.println("\nAfter Updating Age:");
-        s1.displayStudentInfo();
+        FileInputStream in = new FileInputStream("data.bin");
+        int data;
+        while ((data = in.read()) != -1) {
+            System.out.println((char) data);
+        }
+        in.close();
     }
 }
