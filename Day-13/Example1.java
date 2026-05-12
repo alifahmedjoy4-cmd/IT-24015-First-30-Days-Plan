0f98fb1f0ec2cda3b9973b671efe18b40aff212f
Alif Ahmed Joy <alifahmedjoy4@gmail.com>
2026-05-13 00:00:49 +0600

    Update Example1.java

--- Day-13/Example1.java
+++ Day-13/Example1.java
@@ -1,14 +1,25 @@
-import java.util.TreeMap;
+import java.util.ArrayList;
+
+class Product {
+    int id;
+    String name;
+
+    Product(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
 
 public class Example1 {
     public static void main(String[] args) {
-        TreeMap<String, Integer> map = new TreeMap<>();
-        map.put("Watermelon", 3);
-        map.put("Lychee", 1);
-        map.put("Mango", 2);
+        ArrayList<Product> cart = new ArrayList<>();
 
-        for (String key : map.keySet()) {
-            System.out.println(key + " " + map.get(key));
+        cart.add(new Product(1, "Laptop"));
+        cart.add(new Product(2, "Mouse"));
+        cart.add(new Product(3, "Keyboard"));
+
+        for (Product p : cart) {
+            System.out.println(p.id + " " + p.name);
         }
     }
 }
